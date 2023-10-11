3721ea33aa1ff4a9f39d30247ba5439dbd150fb8
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-10-11 21:13:15 +0530

    Login at frontend using token
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- quizadda_client/src/app/pages/login/login.component.ts
+++ quizadda_client/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginData = {
+    username:'',
+    password:''
+  }
+
+  constructor(private snack:MatSnackBar, private login:LoginService) {}
+
+  formSubmit() {
+    console.log('login btn clicked');
+
+    if(this.loginData.username.trim() == '' || this.loginData.username == null) {
+      this.snack.open("Username is required !!", '', {
+        duration: 3000
+      })
+      return;
+    }
+    if(this.loginData.password.trim() == '' || this.loginData.password == null) {
+      this.snack.open("Password is required !!", '', {
+        duration: 3000
+      })
+      return;
+    }
+
+    // request to server to generate token
+    this.login.generateToken(this.loginData).subscribe(
+      (data: any) => {
+        console.log('Success');
+        console.log(data);
+      },
+      (error)=>{
+        console.log("Error !");
+        console.log(error);
+      }
+    )
+  }
 }
