9cbf6f5f91d5d3ff1a0231a2850dac01434d6fdb
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-11-03 18:48:32 +0530

    Evaluating marks on submitting the quiz
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- quizadda_client/src/app/pages/user/start-page/start-page.component.ts
+++ quizadda_client/src/app/pages/user/start-page/start-page.component.ts
@@ -13,6 +13,11 @@ export class StartPageComponent {
   quizId: number | any;
   questions: any;
 
+  marksGot = 0;
+  correctAnswers = 0;
+  attempted = 0;
+  isSubmitted = false;
+
   constructor(
     private _locationSt: LocationStrategy,
     private _route: ActivatedRoute,
@@ -36,11 +41,44 @@ export class StartPageComponent {
     this._question.getQuestionsOfQuizForUser(quizId).subscribe(
       (data) => {
         this.questions = data;
+        this.questions.forEach((ques: any) => {
+          ques['chosenAnswer'] = ''
+        });
       },
       (error) => {
         console.log(error);
         Swal.fire('Error !!', 'Error while loading data', 'error');
       }
     )
+  }
+
+  submitQuiz() {
+    Swal.fire({
+      title: 'Do you want to submit the quiz?',
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Submit'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // calculations
+        this.isSubmitted = true;
+        this.questions.forEach((ques:any) => {
+          if(ques.chosenAnswer == ques.answer) {
+            this.correctAnswers++;
+            let oneQuesMarks = ques.quiz.maxMarks/this.questions.length;
+            this.marksGot += oneQuesMarks;
+          }
+          if(ques.chosenAnswer.trim() != "") {
+            this.attempted++;
+          }
+        })
+
+        console.log("Correct Answers : "+this.correctAnswers);
+        console.log("Marks Got : "+this.marksGot);
+        console.log("Question Attempted : "+this.attempted);
+      }
+    })
   }
 }
